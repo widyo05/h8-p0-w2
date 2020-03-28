1446f2b8b557b7391a5b63f951e20d170f850aaa
widyo05 <widyo05>
2020-03-28 15:00:49 +0700

    exercises 10

---
+++ exercises10.js
@@ -0,0 +1,9 @@
+function Banding(angka1, angka2) {
+    if (angka1 === angka2) {
+        return true
+    } else {
+        return false;
+    }
+}
+var tampil = Banding(5, 5);
+console.log(tampil);
