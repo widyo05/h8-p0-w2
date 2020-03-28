42f9713ecb8da52c5da3d3795e6cb7d17d707d84
widyo05 <widyo05>
2020-03-28 13:13:54 +0700

    exercises 4

---
+++ exercises4.js
@@ -0,0 +1,54 @@
+var tanggal = 25;
+var bulan = 12;
+var tahun = 1987;
+
+switch (bulan) {
+    case 1:
+        bulan = 'Januari';
+        console.log(tanggal + ' ' + bulan + ' ' + tahun);
+        break;
+    case 2:
+        bulan = 'Februari';
+        console.log(tanggal + ' ' + bulan + ' ' + tahun);
+        break;
+    case 3:
+        bulan = 'Maret';
+        console.log(tanggal + ' ' + bulan + ' ' + tahun);
+        break;
+    case 4:
+        bulan = 'April';
+        console.log(tanggal + ' ' + bulan + ' ' + tahun);
+        break;
+    case 5:
+        bulan = 'Mei';
+        console.log(tanggal + ' ' + bulan + ' ' + tahun);
+        break;
+    case 6:
+        bulan = 'Juni';
+        console.log(tanggal + ' ' + bulan + ' ' + tahun);
+        break;
+    case 7:
+        bulan = 'Juli';
+        console.log(tanggal + ' ' + bulan + ' ' + tahun);
+        break;
+    case 8:
+        bulan = 'Agustus';
+        console.log(tanggal + ' ' + bulan + ' ' + tahun);
+        break;
+    case 9:
+        bulan = 'September';
+        console.log(tanggal + ' ' + bulan + ' ' + tahun);
+        break;
+    case 10:
+        bulan = 'Oktober';
+        console.log(tanggal + ' ' + bulan + '' + tahun);
+        break;
+    case 11:
+        bulan = 'November';
+        console.log(tanggal + ' ' + bulan + '' + tahun);
+        break;
+    case 12:
+        bulan = 'Desember';
+        console.log(tanggal + ' ' + bulan + '' + tahun);
+        break;
+}
